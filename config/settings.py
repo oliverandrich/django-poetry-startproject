b836ba1b5ee0dd7fed97bc1eff835c5ec6828193
Oliver Andrich <oliver@andrich.me>
2023-03-04 23:48:00 +0100

    Fixed i18n and initial migration.

--- config/settings.py
+++ config/settings.py
@@ -145,10 +145,10 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
-LANGUAGE_CODE = "de-DE"
+LANGUAGE_CODE = "en"
 LANGUAGES = [
-    ("de", _("Deutsch")),
-    ("en", _("Englisch")),
+    ("en", _("English")),
+    ("de", _("German")),
 ]
 TIME_ZONE = "UTC"
 USE_I18N = True
